4816a369434fdd6c7d47e07111a8e039edfb57e7
Andrew Hoang <hoangaatrox23504@gmail.com>
2024-12-28 09:20:11 +0700

    migrate to home

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,17 +1,22 @@
 <template>
   <v-app class="container">
     <v-main class="d-flex justify-center align-center flex-column">
-      <div v-if="user" class="text-center">
-        <h1 class="display-1 mb-4">Welcome, {{ user.name }} <span><img
-              src="https://em-content.zobj.net/source/apple/391/hear-no-evil-monkey_1f649.png" alt="kon khi kho"
-              width="60"></span></h1>
-        <v-btn variant="tonal" color="deep-purple-darken-1" @click="logout">
-          <v-icon class="mr-2">mdi-logout</v-icon>
-          <span style="user-select: none !important;">Logout</span>
-        </v-btn>
+      <div v-if="loading" class="text-center">
+        <div>Loading...</div>
       </div>
-      <div v-else class="text-center">
-        <h1 class="display-1">Welcome, Guest!</h1>
+      <div v-else>
+        <div v-if="user" class="text-center">
+          <h1 class="display-1 mb-4">Welcome, {{ user.name }} <span><img
+                src="https://em-content.zobj.net/source/apple/391/hear-no-evil-monkey_1f649.png" alt="kon khi kho"
+                width="60"></span></h1>
+          <v-btn variant="tonal" color="deep-purple-darken-1" @click="logout">
+            <v-icon class="mr-2">mdi-logout</v-icon>
+            <span style="user-select: none !important;">Logout</span>
+          </v-btn>
+        </div>
+        <div v-else class="text-center">
+          <h1 class="display-1">Welcome, Guest!</h1>
+        </div>
       </div>
     </v-main>
   </v-app>
@@ -21,16 +26,39 @@
 import { ref, onMounted } from 'vue';
 
 const user = ref(null);
+const loading = ref(true);
 
 onMounted(() => {
-  const userData = localStorage.getItem('user');
-  if (userData) {
-    user.value = JSON.parse(userData);
+  const hash = window.location.hash.substring(1);
+  const params = new URLSearchParams(hash);
+  const accessToken = params.get('access_token');
+
+  if (accessToken) {
+    fetch('https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=' + accessToken)
+      .then(response => response.json())
+      .then(userData => {
+        localStorage.setItem('user', JSON.stringify(userData));
+        user.value = userData;
+        window.location.hash = ''; // Clear the hash
+      })
+      .catch(error => {
+        console.error('Error fetching user info:', error);
+      })
+      .finally(() => {
+        loading.value = false;
+      });
+  } else {
+    const userData = localStorage.getItem('user');
+    if (userData) {
+      user.value = JSON.parse(userData);
+    }
+    loading.value = false;
   }
 });
 
 const logout = () => {
   localStorage.removeItem('user');
+  user.value = null;
   window.location.href = '/';
 }
 </script>
